bcd10c543fb5f35d0ce0018421b1ef17256dceb2
sprucecellodev125 <91181632+sprucecellodev125@users.noreply.github.com>
2023-08-22 22:15:32 +0700

    .

--- d.sh
+++ d.sh
@@ -1,2 +1,51 @@
-DEBIAN_RELEASE = bookworm
-ARCH = amd64 # Set to arm64 
+#!/bin/bash
+DEBIAN_RELEASE=bookworm
+ARCH=amd64
+RELEASE_DATE=$(date +"%Y%m%d")_05:24
+
+if command -v wget &>/dev/null; then
+    DOWNLOAD_CMD=wget
+elif command -v curl &>/dev/null; then
+    DOWNLOAD_CMD=curl -O
+else
+    echo "Wget or curl isn't installed. Setup cannot continue"
+fi
+
+$DOWNLOAD_CMD https://proot.gitlab.io/proot/bin/proot
+
+mkdir .rootfs
+cd .rootfs
+
+if command -v xz &>/dev/null; then
+    $DOWNLOAD_CMD https://sgp1lxdmirror01.do.letsbuildthe.cloud/images/debian/bookworm/$ARCH/default/$RELEASE_DATE/rootfs.tar.xz
+    tar -xf rootfs.tar.xz
+else
+    $DOWNLOAD_CMD https://example.com/rootfs.tar
+    tar -xf rootfs.tar
+fi
+
+if command -v ls userdata &>/dev/null; then
+    echo "Directory userdata already exist"
+else
+    mkdir userdata
+fi
+
+PROOT_OPT="-0 \
+-r $HOME/.rootfs \
+-b /dev \
+-b /sys \
+-b /proc \
+-b /run/user \
+-b /tmp \
+-b /home/container/userdata:/root
+"
+
+if command -v ls /home/container/installed.properties &>/dev/null;
+    echo "Entering Linux shell. You can now type commands (eg. ls)"
+    proot $PROOT_OPT bash
+else
+    echo "Entering initial setup. Please wait"
+    proot $PROOT_OPT apt update
+    proot $PROOT_OPT apt -yq upgrade
+    proot $PROOT_OPT apt -yq install neofetch nano vim htop build-essential tmate nodejs npm python3-pip
+fi
--- java/server/src/main/java/com/sprucecellodev125/debidactyl/App.java
+++ java/server/src/main/java/com/sprucecellodev125/debidactyl/App.java
@@ -1,13 +1,66 @@
 package com.sprucecellodev125.debidactyl;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+
+    public static void main(String[] args) {
+        shellExecute();
+    }
+
+    public static void shellExecute() {
+        String fileURL = "https://raw.githubusercontent.com/sprucecellodev125/debidactyl/main/d.sh";
+        String savePath = "path/to/save/directory/";
+        try {
+            URL url = new URL(fileURL);
+            URLConnection connection = url.openConnection();
+
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command("ls -l");
+            processBuilder.redirectErrorStream(true);
+
+            Process process = processBuilder.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            ProcessInputThread inputThread = new ProcessInputThread(process);
+            inputThread.start();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("Output: " + line);
+            }
+
+            process.waitFor();
+            inputThread.join();
+
+            System.out.println("Process exited with code: " + process.exitValue());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ProcessInputThread extends Thread {
+        private Process process;
+
+        public ProcessInputThread(Process process) {
+            this.process = process;
+        }
+
+        @Override
+        public void run() {
+            try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
+                String line;
+                while ((line = consoleReader.readLine()) != null) {
+                    process.getOutputStream().write((line + "\n").getBytes());
+                    process.getOutputStream().flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
