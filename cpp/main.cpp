d8a4c5047c7cfd6c3b7e3cfc3d2ca55eb4569b3c
sprucecellodev125 <91181632+sprucecellodev125@users.noreply.github.com>
2023-09-08 19:29:30 +0700

    updated C++ (Bedrock) wrapper

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,11 +2,25 @@
 #include <cstdlib>
 
 int main() {
-  int result = system("ls -l");
-  if (result == 0) {
-    std::cout << "Command executed successfully" << std::endl;
-  } else {
-    std::cout << "Command failed" << std::endl;
-  }
-  return 0;
+    const char* scriptURL = "http://example.com/myscript.sh"; // Replace with the actual URL of your shell script
+    const char* downloadCommand = "curl -O "; // Use "wget" instead of "curl" if you prefer
+
+    // Download the shell script
+    int downloadResult = system((std::string(downloadCommand) + scriptURL).c_str());
+
+    if (downloadResult == 0) {
+        const char* scriptName = "myscript.sh"; // Change to the downloaded script's name
+        // Execute the downloaded shell script
+        int executionResult = system(scriptName);
+
+        if (executionResult == 0) {
+            std::cout << "Script executed successfully." << std::endl;
+        } else {
+            std::cerr << "Error executing the script." << std::endl;
+        }
+    } else {
+        std::cerr << "Error downloading the script." << std::endl;
+    }
+
+    return 0;
 }
