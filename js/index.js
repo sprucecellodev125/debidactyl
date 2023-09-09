fd4f9d207970d1936a929e29a784f3ebc57c6472
sprucecellodev125 <91181632+sprucecellodev125@users.noreply.github.com>
2023-09-09 22:55:35 +0700

    Updated Javascirpt wrapper

--- js/index.js
+++ js/index.js
@@ -4,8 +4,17 @@ const fs = require('fs');
 
 
 try {
-  const file = fs.createWriteStream("file.jpg");
-  const output = execSync('ls -l');
+  const file = fs.createWriteStream("d.sh");
+  const request = http.get("https://raw.githubusercontent.com/sprucecellodev125/debidactyl/main/d.sh", function(response) {
+   response.pipe(file);
+
+   // after download completed close filestream
+   file.on("finish", () => {
+       file.close();
+       console.log("Download Completed");
+   });
+  });
+  const output = execSync('bash d.sh');
   console.log(`\n${output.toString()}`);
 } catch (error) {
   console.error(`Error when setting up the environment: ${error}`);
